3120fa6965ac003603696aaed4a43d9b2712dee3
NFoithong <n.foithong1983@gmail.com>
2022-04-23 03:01:44 -0400

    Add Mutations for Thoughts, Friends, and Reactions

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
-
+const { authMiddleware } = require('./utils/auth');
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
@@ -14,8 +14,8 @@ const startServer = async() => {
     const server = new ApolloServer({
         typeDefs,
         resolvers,
-        // context: authMiddleware
-        // context: ({ req }) => req.headers
+        context: authMiddleware
+            // context: ({ req }) => req.headers
     });
 
     // Start the Apollo server
