7e228aee79b387d5f431d2c54ef699e9ea43545d
NFoithong <n.foithong1983@gmail.com>
2022-04-23 15:10:00 -0400

    Create ThoughtList Component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 // importing the useQuery Hook from Apollo Client. This will allow us to make requests to the GraphQL server
 import { useQuery } from '@apollo/client';
 import {QUERY_THOUGHTS} from '../utils/queries';
+import ThoughtList from '../components/ThoughtList';
 
 const Home = () => {
    // use useQuery hook to make query request
@@ -14,7 +15,13 @@ const Home = () => {
   return (
     <main>
       <div className='flex-row justify-space-between'>
-        <div className='col-12 mb-3'>{/* PRINT THOUGHT LIST */}</div>
+        <div className='col-12 mb-3'>{/* PRINT THOUGHT LIST */}
+          {loading ? (
+            <div>Loading...</div>
+          ) : (
+            <ThoughtList thoughts={thoughts} title="Some Feed for Thought(s)..." />
+          )}
+        </div>
       </div>
     </main>
   );
