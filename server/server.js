6bad3c8e5a1fac3abedb5701be9d0fcadc75d6a8
NFoithong <n.foithong1983@gmail.com>
2022-04-23 21:09:58 -0400

    Set Up Application Root Functionality

--- server/server.js
+++ server/server.js
@@ -1,3 +1,6 @@
+// update the back-end server's code to serve up the React front-end code in production.
+const path = require('path');
+
 const express = require('express');
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
@@ -33,6 +36,16 @@ startServer();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+//we created was a wildcard GET route for the server. In other words, if we make a GET request to any location on the server
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
     app.listen(PORT, () => {
